f95e245cc529721432d7f91b2b1704d2f9c6ad84
choiaryeon03544 <choiaryeon03544@gmail.com>
2016-03-21 11:32:13 +0900

    update cohort.sql

--- update cohort.sql
+++ update cohort.sql
@@ -3,7 +3,7 @@
 DROP TEMPORARY TABLE IF EXISTS first_rental;
 CREATE TEMPORARY TABLE first_rental
 
-SELECT customer_id, MIN(rental_date)
+SELECT customer_id, MIN(rental_date) first_time
 
 FROM rental
 
@@ -13,4 +13,18 @@ GROUP BY customer_id
 
 SELECT * FROM first_rental
 
+;
+
+# Cohort Size
+
+DROP TEMPORARY TABLE IF EXISTS cohort_size;
+CREATE TEMPORARY TABLE cohort_size
+
+SELECT 
+		LEFT(first_time,7)Month,
+		COUNT(*) num
+
+FROM first_rental
+
+GROUP BY 1
 ;
