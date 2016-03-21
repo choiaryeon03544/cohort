20fcabc4ee0f63f97853a92399b67c1fb2c34517
choiaryeon03544 <choiaryeon03544@gmail.com>
2016-03-21 11:19:14 +0900

    update cohort.sql

---
+++ update cohort.sql
@@ -0,0 +1,16 @@
+# Customer's First Rental 
+
+DROP TEMPORARY TABLE IF EXISTS first_rental;
+CREATE TEMPORARY TABLE first_rental
+
+SELECT customer_id, MIN(rental_date)
+
+FROM rental
+
+GROUP BY customer_id
+
+;
+
+SELECT * FROM first_rental
+
+;
